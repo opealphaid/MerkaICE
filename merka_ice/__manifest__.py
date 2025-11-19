792e67bc54607dd39d04a9ceb556c1ad9b7de52c
Sergio Luna <SLuna@alphasys.com.bo>
2025-11-19 10:28:22 -0400

    [ADD] Base

---
+++ merka_ice/__manifest__.py
@@ -0,0 +1,15 @@
+# __manifest__.py en config_siat
+{
+    "name": "",
+    "version": "1.2",
+
+    "depends": [
+
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+
+    ],
+    "installable": True,
+    "application": False,
+}
